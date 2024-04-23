601c4b37de3c97a71e7278fba8f45f8c923d05f9
Petar Ramic <petarramic215@gmail.com>
2024-04-23 16:37:38 +0200

    feat: create basis theme

---
+++ src/custom/plugins/AlviTheme/src/AlviTheme.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace AlviTheme;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class AlviTheme extends Plugin implements ThemeInterface
+{
+}
